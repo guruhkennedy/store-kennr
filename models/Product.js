d83ec1c452801733b791be8dc512c3e5cd09868d
guruhkennedy <guruh52480@gmail.com>
2025-06-02 16:00:07 +0700

    update ui

--- models/Product.js
+++ models/Product.js
@@ -6,7 +6,7 @@ const ProductSchema = new mongoose.Schema({
     brand: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    discountPrice: { type: Number, required: true },
+    discountPrice: { type: Number, default: null },
     image_url: { type: String, required: true },
     stock: { type: Number, default: 0, required: true },
     created_at: { type: Date, default: Date.now }
